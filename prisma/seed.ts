9493cbbe1ec31ea0147afbceabedf683548452fe
Lean Junio <leanjunio@outlook.com>
2023-02-08 20:26:17 -0500

    feat: auto generate 10 seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,33 @@
+import { randEmail, randFullName, randVehicle } from "@ngneat/falso";
 import { PrismaClient } from "@prisma/client";
+import vinGenerator from "vin-generator";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const email = "aaron@autoshop.com";
+  const NUM_SEED_USERS = 10;
 
-  await prisma.user.upsert({
-    where: { email },
-    update: {},
-    create: {
-      email,
-      name: "Aaron Mist",
-      vehicles: {
-        create: {
-          model: "Corolla",
-          vin: "JH4DC4330RS802540",
+  for (let i = 0; i < NUM_SEED_USERS; ++i) {
+    const email = randEmail();
+    const name = randFullName();
+    const vehicleModel = randVehicle();
+    const vin = vinGenerator.generateVin();
+
+    await prisma.user.upsert({
+      where: { email },
+      update: {},
+      create: {
+        email,
+        name,
+        vehicles: {
+          create: {
+            model: vehicleModel,
+            vin,
+          },
         },
       },
-    },
-  });
+    });
+  }
 }
 main()
   .then(async () => {
