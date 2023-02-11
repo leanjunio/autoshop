7f022465bbef8abc38f13f7fc5cf1d4c79b11173
Lean Junio <leanjunio@outlook.com>
2023-02-11 14:55:20 -0500

    create hash compare func

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -3,3 +3,7 @@ import * as bcrypt from "bcrypt";
 export function hash(password: string) {
   return bcrypt.hashSync(password, 10);
 }
+
+export function compare(password: string, hash: string) {
+  return bcrypt.compare(password, hash);
+}
