309bdc0c3bd22f29a7968da76a5147fc4b9ba7b1
Lean Junio <leanjunio@outlook.com>
2023-02-11 18:57:59 -0500

    use faker package data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Vehicle } from "@prisma/client";
 import { users } from "./seed/users";
-import { vehicles } from "./seed/vehicles";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
+
+type RandomVehicle = Omit<Vehicle, "userId" | "id">;
+
+function createRandomVehicle(): RandomVehicle {
+  return {
+    plate_number: faker.random.alphaNumeric(7, {
+      casing: "upper",
+    }),
+    manufacturer: faker.vehicle.manufacturer(),
+    model: faker.vehicle.model(),
+    ac: faker.datatype.boolean(),
+    vin: faker.vehicle.vin(),
+    body: faker.vehicle.type(),
+    driver_name: faker.name.fullName(),
+    purchase_year: faker.datatype.number({
+      max: 2023,
+      min: 1995,
+    }),
+    initial_cost: faker.finance.amount(20000, 70000),
+    avg_yearly_cost: faker.finance.amount(1000, 4000),
+    transmission_type: "Automatic",
+    manufacture_year: faker.datatype.number({
+      max: 2023,
+      min: 1995,
+    }),
+    engine_size: 8,
+  };
+}
 
 async function main() {
   await Promise.all(
@@ -13,7 +41,7 @@ async function main() {
         create: {
           ...user,
           vehicles: {
-            create: vehicles[i],
+            create: createRandomVehicle(),
           },
         },
       });
