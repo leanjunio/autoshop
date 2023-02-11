b20f78b498267fe2283376f29a96ef43004526ff
Lean Junio <leanjunio@outlook.com>
2023-02-11 14:54:31 -0500

    change default seed password
    Change from `passwordHash` to `password`

--- prisma/seed/users.ts
+++ prisma/seed/users.ts
@@ -5,18 +5,18 @@ export const users = [
     id: "c5c4dd15-0701-47e9-b606-8a67e234fb07",
     email: "tom@autoshop.com",
     name: "Tom Roberts",
-    passwordHash: hash("passwordHash"),
+    passwordHash: hash("password"),
   },
   {
     id: "87597ae1-7241-41ba-bafb-7f82acc1905f",
     email: "adam@autoshop.com",
     name: "Adam Page",
-    passwordHash: hash("passwordHash"),
+    passwordHash: hash("password"),
   },
   {
     id: "bf0ecb27-bc7c-467a-b26c-ab286117b593",
     email: "mark@autoshop.com",
     name: "Mark Stevens",
-    passwordHash: hash("passwordHash"),
+    passwordHash: hash("password"),
   },
 ];
