0c870125a6c025b48bdff3a0568099e9ba5f8192
Lean Junio <leanjunio@outlook.com>
2023-02-11 15:47:41 -0500

    add guard for vehicles page

--- src/pages/vehicles/index.tsx
+++ src/pages/vehicles/index.tsx
@@ -1,22 +1,59 @@
 import Layout from "@/components/layout";
 import VehicleRow from "@/components/vehicles/vehicle/row";
 import prisma from "@/lib/prisma";
-import { InferGetServerSidePropsType } from "next";
+import { Vehicle } from "@prisma/client";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
 
-export async function getServerSideProps() {
+type ResponseType = {
+  vehicles: Vehicle[];
+};
+
+export const getServerSideProps: GetServerSideProps<ResponseType> = async ({
+  req,
+  res,
+}) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.statusCode = 403;
+    return { props: { vehicles: [] } };
+  }
+
   const vehicles = await prisma.vehicle.findMany();
   return {
     props: { vehicles },
   };
-}
+};
 
 type VehiclesPageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 export default function Vehicles({ vehicles }: VehiclesPageProps) {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <>
+        <Head>
+          <title>Vehicles</title>
+        </Head>
+        <Layout>
+          <div>
+            <div className="mb-12">
+              <p className="text-5xl font-bold">All Vehicles</p>
+            </div>
+            <p className="text-lg">
+              You need to be authenticated to view this page.
+            </p>
+          </div>
+        </Layout>
+      </>
+    );
+  }
   return (
     <>
       <Head>
-        <title>Vehicles Page</title>
+        <title>Vehicles</title>
       </Head>
       <Layout>
         <div>
