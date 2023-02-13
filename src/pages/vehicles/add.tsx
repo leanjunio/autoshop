3c41e6d360838e28ce19baf7b77393351cd1e723
Lean Junio <leanjunio@outlook.com>
2023-02-12 22:21:57 -0500

    add zod
    used for form validation in this case

--- src/pages/vehicles/add.tsx
+++ src/pages/vehicles/add.tsx
@@ -1,7 +1,24 @@
 import Layout from "@/components/layout";
 import Head from "next/head";
+import { z } from "zod";
+
+const validationSchema = z.object({
+  plate_number: z.string(),
+  vin: z.string(),
+  model: z.string().optional(),
+  manufacturer: z.string().optional(),
+  body: z.string().optional(),
+  transmittion_type: z.string().optional(),
+  engine_size: z.number().optional(),
+  manufacture_year: z.number().optional(),
+  purchase_year: z.number().optional(),
+  driver_name: z.string().optional(),
+  ac: z.boolean().optional(),
+});
 
 export default function AddVehiclePage() {
+  function onSubmit() {}
+
   return (
     <>
       <Head>
