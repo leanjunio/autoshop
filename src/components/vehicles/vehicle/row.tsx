b6e4d93e52ee631730ec25c76fb4b8334b5f2671
Lean Junio <leanjunio@outlook.com>
2023-02-12 21:02:38 -0500

    modify vehicle row information

--- src/components/vehicles/vehicle/row.tsx
+++ src/components/vehicles/vehicle/row.tsx
@@ -13,9 +13,12 @@ export default function VehicleRow({ vehicle }: VehicleRowProps) {
   return (
     <div
       onClick={() => goToVehiclePage(vehicle.id)}
-      className="m-5 p-5 border rounded-md hover:cursor-pointer hover:bg-accent duration-700"
+      className="m-5 p-5  rounded-md hover:cursor-pointer hover:bg-accent duration-700"
     >
-      <h1 className="font-semibold">{vehicle.model}</h1>
+      <h1 className="font-semibold">
+        {vehicle.manufacture_year} {vehicle.manufacturer} {vehicle.model} -{" "}
+        {vehicle.plate_number}
+      </h1>
       <p>{vehicle.vin}</p>
     </div>
   );
