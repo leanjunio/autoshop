236ec81d812277374f78363c96e9d97a76100f0c
Lean Junio <leanjunio@outlook.com>
2023-02-20 23:37:42 -0500

    install toast notifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 
 export default function App({
   Component,
@@ -9,6 +10,7 @@ export default function App({
   return (
     <div data-theme="autumn">
       <SessionProvider session={session}>
+        <Toaster />
         <Component {...pageProps} />
       </SessionProvider>
     </div>
