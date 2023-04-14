bb0aadbeed9d4a10fb8abe8e07e1747ec5698e07
Lean Junio <leanjunio@live.com>
2023-04-14 18:21:09 -0400

    remove view reports

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,28 +1,23 @@
 import { User } from "@prisma/client";
-import { useRouter } from "next/router";
+import { FiEdit } from "react-icons/fi";
 
 type SidebarProps = {
   user: Pick<User, "name" | "phone_number" | "email">;
 }
 
 export default function Sidebar({ user }: SidebarProps) {
-  const router = useRouter();
-
-  function goToReportsPage() {
-    router.push(`/reports`);
-  }
-
   return (
     <div className="w-1/5 py-28 px-24 flex flex-col gap-y-10">
       <div>
-        <p className="my-2 font-bold text-xl">{user.name}</p>
+        <div className="flex items-center justify-between flex-row w-full">
+          <p className="my-2 font-bold text-xl">{user.name}</p>
+          <button className="hover:opacity-80">
+            <FiEdit size={17} />
+          </button>
+        </div>
         <p className="text-sm text-gray-600">{user.phone_number}</p>
         <p className="text-sm text-gray-600">{user.email}</p>
-        <button className="my-5 text-sm btn btn-outline btn-xs border-base-200">
-          Edit Profile
-        </button>
       </div>
-      <button onClick={goToReportsPage} className="btn btn-outline btn-accent">View Reports</button>
     </div>
   )
 }
