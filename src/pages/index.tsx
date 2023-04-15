85f983813b5e3bed56f428665e463bed030b3677
Lean Junio <leanjunio@live.com>
2023-04-14 21:35:06 -0400

    add responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,34 +48,34 @@ export default function HomePage() {
           <div className="my-10 text-center font-extrabold">
             <p className="text-3xl">We offer a complete range of services</p>
           </div>
-          <div className="grid grid-rows-2 grid-cols-3 px-40 w-3/4 text-center">
-            <div className="flex flex-col p-10">
-              <FiDollarSign className="my-5 w-full text-accent" size={40} />
+          <div className="grid grid-rows-6 grid-cols-1 md:grid-rows-2 md:grid-cols-3 md:px-40 w-3/4 text-center">
+            <div className="flex flex-col md:p-10">
+              <FiDollarSign className="mb-5 w-full text-accent" size={40} />
               <p className="font-bold text-lg my-4">Enhance resale</p>
               <p className="text-sm">Print your personalized reports for top dollar resale value on your vehicle</p>
             </div>
             <div className="flex flex-col p-10">
-              <FiHeart className="my-5 w-full text-accent" size={40} />
+              <FiHeart className="mb-5 w-full text-accent" size={40} />
               <p className="font-bold text-lg my-4">Encourage loyalty</p>
               <p className="text-sm">Easily manage your loyalty program to maintain customer retention</p>
             </div>
             <div className="flex flex-col p-10">
-              <FiShield className="my-5 w-full text-accent" size={40} />
+              <FiShield className="mb-5 w-full text-accent" size={40} />
               <p className="font-bold text-lg my-4">Establish trust</p>
               <p className="text-sm">We only acquire vehicle data with permission from our customers</p>
             </div>
             <div className="flex flex-col p-10">
-              <FiGlobe className="my-5 w-full text-accent" size={40} />
+              <FiGlobe className="mb-5 w-full text-accent" size={40} />
               <p className="font-bold text-lg my-4">Save the planet</p>
               <p className="text-sm">Monitor vehicle cost to prolong vehicle life, reducing manufacturing production to slow down climate change</p>
             </div>
             <div className="flex flex-col p-10">
-              <FiAward className="my-5 w-full text-accent" size={40} />
+              <FiAward className="mb-5 w-full text-accent" size={40} />
               <p className="font-bold text-lg my-4">Compare and compete</p>
               <p className="text-sm">Compare customer spending, shop performance, and common repair issues to other shops within the network</p>
             </div>
             <div className="flex flex-col p-10">
-              <FiClock className="my-5 w-full text-accent" size={40} />
+              <FiClock className="mb-5 w-full text-accent" size={40} />
               <p className="font-bold text-lg my-4">Get your time back</p>
               <p className="text-sm">Print business use reports for; insuance costs, license fees, fuel cost, mechcanical services. repairs and more!</p>
             </div>
