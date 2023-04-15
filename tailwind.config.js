24b0864080f31b0067064340b6ef6d1610061b97
Lean Junio <leanjunio@live.com>
2023-04-14 20:05:00 -0400

    upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [require("daisyui")],
   daisyui: {
     base: true,
